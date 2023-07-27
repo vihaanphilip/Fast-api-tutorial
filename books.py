b2b75adf8cd8bbcbd7d81ddb6723a4627d501659
Vihaan Philip <viha0001@student.monash.edu>
2023-07-27 12:55:29 +0800

    Created booksapi

---
+++ books.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=100)
+
+@app.get("/{name}")
+def read_api(name: str):
+    return {"Welcome": name}
