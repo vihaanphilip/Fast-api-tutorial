8a805047fea55e03632421afa87c97ba88958c03
Vihaan Philip <viha0001@student.monash.edu>
2023-07-31 17:22:07 +0800

    update delete method to work with database

--- books.py
+++ books.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
-from uuid import UUID
 
 import models
 from database import engine, SessionLocal
@@ -49,29 +48,52 @@ def create_book(book: Book, db: Session = Depends(get_db)):
     return book
 
 @app.put("/{book_id}")
-def update_book(book_id: UUID, book: Book):
-    counter = 0
+def update_book(book_id: int, book: Book, db: Session = Depends(get_db)):
+    # counter = 0
 
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            BOOKS[counter-1] = book
-            return BOOKS[counter-1]
-    raise HTTPException(
-        status_code=404, 
-        detail=f"ID {book_id} not found"
-    )
+    # for x in BOOKS:
+    #     counter += 1
+    #     if x.id == book_id:
+    #         BOOKS[counter-1] = book
+    #         return BOOKS[counter-1]
+
+    book_model = db.query(models.Book).filter(models.Book.id == book_id).first()
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404, 
+            detail=f"ID {book_id} not found"
+        )
+    
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+
+    db.add(book_model)
+    db.commit()
+
+    return book
 
 @app.delete("/{book_id}")
-def delete_book(book_id: UUID):
-    counter = 0
+def delete_book(book_id: int, db: Session = Depends(get_db)):
+    # counter = 0
 
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            del BOOKS[counter-1]
-            return {"message": "Deleted book"}
-    raise HTTPException(
-        status_code=404,
-        detail=f"ID {book_id} not found"
-    )
+    # for x in BOOKS:
+    #     counter += 1
+    #     if x.id == book_id:
+    #         del BOOKS[counter-1]
+    #         return {"message": "Deleted book"}
+
+    book_model = db.query(models.Book).filter(models.Book.id == book_id).first()
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {book_id} not found"
+        )
+    
+    db.query(models.Book).filter(models.Book.id == book_id).delete()
+
+    db.commit()
+
